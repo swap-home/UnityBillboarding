83dc4612897bb657b0794a0f03aa76a4ca4f19ae
swaparcher <hydrazhang@hotmail.com>
2015-08-20 16:39:08 -0700

    init

---
+++ Assets/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+	public Camera m_Camera;
+
+	void Start()
+	{
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		if (renderer) {
+			//renderer.sortingLayerName = "Z";
+			//renderer.sortingOrder = (int)transform.position.z;
+		}
+	}
+
+	void Update()
+	{
+		transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+		                 m_Camera.transform.rotation * Vector3.up);
+	}
+}
